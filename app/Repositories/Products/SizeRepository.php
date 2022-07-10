adb39ba2b999d7b8f0ec0fb659c28fbdc7647d7a
Victor Olave <victorolave1131@gmail.com>
2022-07-09 23:25:00 -0500

    Repository

---
+++ app/Repositories/Products/SizeRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repositories\Products;
+
+class SizeRepository implements \App\Repositories\Interfaces\Products\SizeRepositoryInterface
+{
+
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update(array $data, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function find($id)
+    {
+        // TODO: Implement find() method.
+    }
+}
