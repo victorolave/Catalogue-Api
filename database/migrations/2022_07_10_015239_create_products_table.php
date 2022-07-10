5d6578cecac0db50792e61bf173a1703faf2d82a
Victor Olave <victorolave1131@gmail.com>
2022-07-09 21:02:59 -0500

    Model & Migrations

---
+++ database/migrations/2022_07_10_015239_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('size_id')->references('id')->on('sizes');
+            $table->text('remarks')->nullable();
+            $table->foreignId('brand_id')->references('id')->on('brands');
+            $table->integer('stock')->default(0);
+            $table->date('shipment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
